64cd673cfe9177e709d5d88f0cac2fa9ef2465ef
Carlos <carloschivas023468@gmail.com>
2021-08-23 18:56:38 -0500

    Fragment Manager Activity created

---
+++ app/src/main/java/mx/tec/bancoalimentos/FragmentManager.kt
@@ -0,0 +1,11 @@
+package mx.tec.bancoalimentos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FragmentManager : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_manager)
+    }
+}
