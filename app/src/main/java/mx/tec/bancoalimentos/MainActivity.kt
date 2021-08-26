7457d0050790587897aef0286cfc8816357e0dca
Brenda Castillo <A01632227@itesm.mx>
2021-08-26 14:37:22 -0500

    Pasar a Fragment Manager desde Login Button

--- app/src/main/java/mx/tec/bancoalimentos/MainActivity.kt
+++ app/src/main/java/mx/tec/bancoalimentos/MainActivity.kt
@@ -1,11 +1,20 @@
 package mx.tec.bancoalimentos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val logInBtn : Button = findViewById(R.id.main_logInBtn);
+
+        logInBtn.setOnClickListener(){
+            val intent = Intent(this, FragmentManager::class.java)
+            startActivity(intent)
+        }
     }
 }
