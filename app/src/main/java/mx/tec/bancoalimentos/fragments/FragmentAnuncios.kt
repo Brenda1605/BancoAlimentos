6ba67fb6e1d5884af9bc6850eceb33fce4eb0af5
Carlos David <60150143+carlosdblanco@users.noreply.github.com>
2021-08-30 18:16:23 -0500

    Cambio nombre a fragment_blank_david.xml

--- app/src/main/java/mx/tec/bancoalimentos/fragments/FragmentAnuncios.kt
+++ app/src/main/java/mx/tec/bancoalimentos/fragments/FragmentAnuncios.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BlankFragmentDavid.newInstance] factory method to
+ * Use the [FragmentAnuncios.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BlankFragmentDavid : Fragment() {
+class FragmentAnuncios : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class BlankFragmentDavid : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank_david, container, false)
+        return inflater.inflate(R.layout.fragment_anuncios, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class BlankFragmentDavid : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragmentDavid.
+         * @return A new instance of fragment FragmentAnuncios.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            BlankFragmentDavid().apply {
+            FragmentAnuncios().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
