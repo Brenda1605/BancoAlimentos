4727fc72c71d20eca2d9cc653419b4aa8658a0ba
Brenda Castillo <A01632227@itesm.mx>
2021-09-01 20:04:02 -0500

    Logout functionality

--- app/src/main/java/mx/tec/bancoalimentos/fragments/FragmentProfile.kt
+++ app/src/main/java/mx/tec/bancoalimentos/fragments/FragmentProfile.kt
@@ -1,10 +1,18 @@
 package mx.tec.bancoalimentos.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import mx.tec.bancoalimentos.FragmentManager
+import mx.tec.bancoalimentos.MainActivity
 import mx.tec.bancoalimentos.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -17,10 +25,11 @@ private const val ARG_PARAM2 = "param2"
  * Use the [FragmentProfile.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentProfile : Fragment() {
+class FragmentProfile : Fragment(), View.OnClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var logoutBtn : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +43,11 @@ class FragmentProfile : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view: View = inflater.inflate(R.layout.fragment_profile, container, false)
+        val btn: Button? = view?.findViewById(R.id.profile_logoutBtn)
+        btn?.setOnClickListener(this)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return view
     }
 
     companion object {
@@ -56,5 +68,11 @@ class FragmentProfile : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onClick(view: View?) {
+        Firebase.auth.signOut()
+        val intent = Intent(activity, MainActivity::class.java)
+        activity?.startActivity(intent)
     }
 }
